943fb475aef11698634a2120a44baecced8fd997
dekken <philip.deegan@gmail.com>
2022-03-08 14:01:48 +0100

    init debian/fedora minimal configs

--- build_and_push_image.sh
+++
@@ -1,7 +0,0 @@
-BASEDIR=$(dirname "$0")
-RELEASE=${1:-33}
-REGISTRY=${2:-"129.104.6.165:32219"}
-IMAGE="phare/teamcity-fedora"
-FULL_NAME="${REGISTRY}/${IMAGE}:${RELEASE}"
-$BASEDIR/build_image.sh $RELEASE $REGISTRY
-docker push $FULL_NAME
--- build_image.sh
+++
@@ -1,5 +0,0 @@
-RELEASE=${1:-33}
-REGISTRY=${2:-"129.104.6.165:32219"}
-IMAGE="phare/teamcity-fedora"
-FULL_NAME="${REGISTRY}/${IMAGE}:${RELEASE}"
-docker build --build-arg RELEASE=$RELEASE -t ${FULL_NAME} .
